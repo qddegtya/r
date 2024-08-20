49c8269ff29eeae79f6dd1a91e378cee44e120d9
archer <qddegtya@gmail.com>
2024-08-20 11:03:24 +0800

    refactor: add deps

--- scripts/creator-telescope/index.ts
+++ scripts/creator-telescope/index.ts
@@ -1,8 +1,8 @@
 // TODO
 import { ReadableNode, WriteableNode, Port, Flow } from '@hlang-org/runtime';
-import C from '../../mas/src/nodes/C';
-import T from '../../mas/src/nodes/T';
-import P from '../../mas/src/nodes/P';
+import C from '../../packages/mas/src/nodes/C';
+import T from '../../packages/mas/src/nodes/T';
+import P from '../../packages/mas/src/nodes/P';
 
 // Start
 class StartNode extends ReadableNode {
