5fb268d88b56269a75f0e21b1c453d9e77244060
archer <qddegtya@gmail.com>
2024-08-28 22:55:44 +0800

    feat: add to-zh-cn

--- packages/mas/src/nodes/T.ts
+++ packages/mas/src/nodes/T.ts
@@ -1,12 +1,28 @@
-import { TransformNode } from "@hlang-org/runtime";
+import { TransformNode, Port } from "@hlang-org/runtime";
+import Piscina from "piscina";
+import path from "node:path";
 
 class TransformAgent extends TransformNode {
   constructor(opt) {
     super(opt);
+
+    Port.I("post").attach(this);
+    Port.O("post").attach(this);
   }
 
   _transform($i: any, $o: any): void {
-    
+    console.log("--- 🐝 TransformAgent ---");
+
+    const piscina = new Piscina({
+      filename: path.resolve(__dirname, "../workers/to-zh-cn.ts"),
+    });
+
+    $i("post").receive(async (post) => {
+      // dispatch job to piscina
+      // with default thread pool config
+      const zhCnPost = await piscina.run({ post });
+      $o("post").send(zhCnPost);
+    });
   }
 }
 
