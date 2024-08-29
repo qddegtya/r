33f83e7d69a94583e01b5f8668aebc7f352a9ece
archer <qddegtya@gmail.com>
2024-08-29 15:07:09 +0800

    chore: optimize deps

--- packages/mas/src/workers/to-zh-cn.ts
+++ packages/mas/src/workers/to-zh-cn.ts
@@ -1,33 +1,51 @@
-import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+const OpenAI = require("openai");
+const fs = require("fs");
 
-const chatModel = new ChatOpenAI({
-  model: "gpt-4o-mini"
+const client = new OpenAI({
+  apiKey: "",
+  baseURL: "https://api.moonshot.cn/v1",
 });
 
 export default async ({ post }) => {
-  const template =
-    "You will be provided with a sentence in {input_language}, and your task is to translate it into {output_language}.";
-  const humanTemplate = "{text}";
-
-  const chatPrompt = ChatPromptTemplate.fromMessages([
-    ["system", template],
-    ["human", humanTemplate],
-  ]);
-
-  const translatePrompt = await chatPrompt.formatMessages({
-    input_language: "English",
-    output_language: "Chinese",
-    text: "I love programming.",
+  await fs.writeFileSync("./tmp.md", post, {
+    encoding: "utf-8",
   });
 
-  console.log(translatePrompt);
-
-  const response = await chatModel.invoke(translatePrompt, {
-    timeout: 5000,
+  let file_object = await client.files.create({
+    file: fs.createReadStream("./tmp.md"),
+    purpose: "file-extract",
   });
 
-  console.log(response);
+  // 获取结果
+  // file_content = client.files.retrieve_content(file_id=file_object.id)
+  // 注意，之前 retrieve_content api 在最新版本标记了 warning, 可以用下面这行代替
+  // 如果是旧版本，可以用 retrieve_content
+  let file_content = await (await client.files.content(file_object.id)).text();
 
-  return post;
+  // 把它放进请求中
+  let messages = [
+    {
+      role: "system",
+      content: "你是 Kimi 翻译助手，你擅长内容翻译",
+    },
+    {
+      role: "system",
+      content: file_content,
+    },
+    {
+      role: "user",
+      content:
+        "现在，你接收到的输入是一份 Markdown 格式的文件，请仅仅将该文档中的内容翻译成中文，这意味着你必须严格保留原有的 Markdown 格式进行最后的结果输出",
+    },
+  ];
+
+  const completion = await client.chat.completions.create({
+    model: "moonshot-v1-128k",
+    messages: messages,
+    temperature: 0.3,
+  });
+
+  const ret = completion.choices[0].message.content;
+
+  return ret;
 };
