dfb77b7fa1859573f6e745ffea927a592ac1c3ce
archer <qddegtya@gmail.com>
2024-09-04 16:01:14 +0800

    feat: add publish-post

--- packages/mas/src/nodes/P.ts
+++ packages/mas/src/nodes/P.ts
@@ -1,12 +1,24 @@
-import { TransformNode } from "@hlang-org/runtime";
+import { Port, WriteableNode } from "@hlang-org/runtime";
+import Piscina from "piscina";
+import path from "node:path";
 
-class PublishAgent extends TransformNode {
+class PublishAgent extends WriteableNode {
   constructor(opt) {
     super(opt);
+
+    Port.I("post").attach(this);
   }
 
-  _transform($i: any, $o: any): void {
-    
+  _write($i) {
+    $i("post").receive(async (post) => {
+      const piscina = new Piscina({
+        filename: path.resolve(__dirname, "../workers/publish-post.js"),
+      });
+
+      // dispatch job to piscina
+      // with default thread pool config
+      await piscina.run({ post });
+    });
   }
 }
 
